f0e1d8e47aa7c2a3497246808d86d7c48a24ace8
RSC Games <rsc.games.official@gmail.com>
2024-09-09 21:02:18 -0400

    Updated home page and revised site design

--- scripts/autocopy.js
+++ scripts/autocopy.js
@@ -1,3 +1,3 @@
 // Automatically update the copyright notice at the bottom of the page.
 var copynotice = document.getElementsByClassName("copyright")[0];
-copynotice.innerHTML = "&copy 2020-" + new Date().getFullYear() + " RSC Games. All rights reserved.";
+copynotice.innerHTML = "&copy 2020-" + new Date().getFullYear() + " Raine Bond. All rights reserved.";
